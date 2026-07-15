73d1f2f48aa0765e60689679e0e7037fbf8ef39e
maksun0113-cmd <maksun0113@gmail.com>
2026-07-16 00:59:43 +0500

    Add files via upload

---
+++ class7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class class7{
+    public static void main(String[]args){
+        Scanner sc = new
+        Scanner( System.in);
+        double length , width ,
+        area;
+        System.out.print( "Enter length");
+        length = sc.nextDouble();
+        System.out.print( " Enter width");
+      width =  sc.nextDouble();
+        area = length*width;
+        System.out.print("Area = " + area);
+        
+    }    
+}
+    
